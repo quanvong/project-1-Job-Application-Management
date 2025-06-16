1877986edcc07f1b7901e9be3f09215fd1c5f998
nguyenvanhien <n19dcat022@student.ptithcm.edu.vn>
2025-06-17 06:46:36 +0700

    Chinh sua file db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '123456',
-  database: process.env.DB_NAME || 'job_application_management',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
 const pool = mysql.createPool(dbConfig);
